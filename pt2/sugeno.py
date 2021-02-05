3db91c0fd74149631c2dd756042a8f35be098f67
josegfer <macaverde604@gmail.com>
2021-02-05 01:22:54 -0300

    1d sol ex4

--- pt2/sugeno.py
+++ pt2/sugeno.py
@@ -2,17 +2,21 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 class Sugeno():
-    def __init__(consequents, antecedents):
+    def __init__(self, consequents, antecedents):
+        #eh mt noia ficar incomodado com os consequentes antes dos antecedentes?
         assert(len(consequents) == len(antecedents))
         self.consequents = consequents
-        self.antedecents = antecedents
+        self.antecedents = antecedents
 
-    def infer(x):
-        weights = [ant(x) for ant in self.antecedent]
-        consequent_memberships = [con(x) for cor in self.consequents]
+    def infer(self, x):
+        # weights = [ant(x) for ant in self.antecedents]
+        weights = self.antecedents
+        # consequent_memberships = [con(x) for cor in self.consequents]
+        consequent_memberships = self.consequents
 
         output = 0
         for (w, cm) in zip(weights, consequent_memberships):
             output += w*cm
 
         normal_output = output/sum(weights)
+        return normal_output
